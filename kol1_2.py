ea99e2c513e5ee3b15fc695d668688018cf2ed11
jpax1996 <duke.jp.freitas@gmail.com>
2017-03-15 14:34:23 +0100

    Update kol1_2.py

--- kol1_2.py
+++ kol1_2.py
@@ -3,5 +3,5 @@ import kol1 as matrix_2
 matrix_1 = Matrix(4,5,6,7)
 matrix_2 = Matrix(2,2,2,1)
 
-matrix_3 = matrix_2.add_matrix(matrix_1)
-matrix_3 = matrix_2.mult_matrix(matrix_1)
+matrix_3 = matrix_2.add_matrix(matrix_1,matrix_2)
+matrix_3 = matrix_2.mult_matrix(matrix_1,matrix_2)
